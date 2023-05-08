6ae582a8a8c5740fe59f21810b2a45617549ac50
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-08 11:53:23 +1000

    commit tree 1a6285623c225ca3f3ca67024ca111b80f8dfd9b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  
+  def initialize a, b, c 
+    @a = a
+    @b = b
+    @c = c
+  end
+
+
+
+
+  def kind 
+    if (self.a > 0 && self.b > 0 && self.c > 0) && (self.a + self.b > self.c && self.a + self.c > self.b && self.b + self.c > self.a)
+      if self.a == self.b && self.b == self.c 
+        :equilateral
+      elsif self.a == self.b || self.a == self.c || self.b == self.c
+        :isosceles
+      else
+        :scalene 
+      end
+    else
+      raise TriangleError
+    end
+
+  end
+
+
+  class TriangleError < StandardError
+    def message 
+      "Error: not a valid triangle"
+    end
+  end
+
 end
